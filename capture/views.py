b81f8abdd9695f6ceeefa2d62bc0492164f2734d
adjustmyself <adjust_myself@hotmail.com>
2019-10-21 08:38:14 +0800

    first commit

---
+++ capture/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.core.files.base import ContentFile
+import base64
+from django.core.files.storage import FileSystemStorage
+from io import BytesIO
+import json
+# 測試東西 import一堆,之後整理
+# Create your views here.
+
+def home(request):
+	return render(request, 'index.html')
+
+
+@csrf_exempt
+def upload(request):
+	Extension = '.jpg'
+	if request.method == 'POST':
+		dictData = json.loads(request.POST.get('jsonData', ''))
+		photoStrData = dictData['photoData']
+		valueStrData = dictData['valueData']
+		format, imgstr = photoStrData.split(';base64,') 
+		ext = format.split('/')[-1] 
+		data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+		fs = FileSystemStorage()
+		fileName = valueStrData+Extension
+		fs.save(fileName,data)
+	return render(request,'upload.html')
